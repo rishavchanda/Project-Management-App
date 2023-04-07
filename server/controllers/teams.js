b0e93a9131e771b38fc26c8c22aab0c26cce0ca7
rishavchanda <rishavchanda0@gmail.com>
2023-04-07 20:02:04 +0530

    added the url

--- server/controllers/teams.js
+++ server/controllers/teams.js
@@ -167,7 +167,7 @@ export const inviteTeamMember = async (req, res, next) => {
 
     req.app.locals.CODE = await otpGenerator.generate(8, { upperCaseAlphabets: true, specialChars: true, lowerCaseAlphabets: true, digits: true, });
 
-    const link = `http://localhost:3001/team/invite/${req.app.locals.CODE}?teamid=${req.params.id}&userid=${req.body.id}&access=${req.body.access}&role=${req.body.role}`;
+    const link = `${process.env.URL}/team/invite/${req.app.locals.CODE}?teamid=${req.params.id}&userid=${req.body.id}&access=${req.body.access}&role=${req.body.role}`;
 
     const mailBody = `
     <div style="font-family: Poppins, sans-serif; max-width: 600px; margin: 0 auto; background-color: #f9f9f9; padding: 20px; border: 1px solid #ccc; border-radius: 5px;">
